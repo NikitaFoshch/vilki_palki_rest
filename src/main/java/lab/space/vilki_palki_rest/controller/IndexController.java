90674956c42cdea0a77b8998d953796d4fcd4487
WB_777 <antariuzz@gmail.com>
2023-06-29 17:33:44 +0300

    Patch 0.5

--- src/main/java/lab/space/vilki_palki_rest/controller/IndexController.java
+++ src/main/java/lab/space/vilki_palki_rest/controller/IndexController.java
@@ -2,8 +2,10 @@ package lab.space.vilki_palki_rest.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("Vilki_Palki_Api")
 public class IndexController {
     @GetMapping({"/", ""})
     public String redirectAdmin() {
