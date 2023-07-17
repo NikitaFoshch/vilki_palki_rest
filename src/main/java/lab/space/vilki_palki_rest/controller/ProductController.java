9a49d5b06e41ad2d3d870e016ddf96e597a9fe65
WB_777 <antariuzz@gmail.com>
2023-07-17 20:50:42 +0300

    Bug fix

--- src/main/java/lab/space/vilki_palki_rest/controller/ProductController.java
+++ src/main/java/lab/space/vilki_palki_rest/controller/ProductController.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lab.space.vilki_palki_rest.model.product.ProductResponse;
 import lab.space.vilki_palki_rest.service.ProductService;
+import lab.space.vilki_palki_rest.util.ErrorMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +30,11 @@ public class ProductController {
             description = "Enter your value, producTypeId can be 0." +
                     "\n If you enter 0 , then get all products without filtering by type")
     @GetMapping("get-all-products/{productTypeId}/{productCategoryId}")
-    public ResponseEntity<?> getAllProductsByRequest(@Valid @PathVariable Long productTypeId, @PathVariable Long productCategoryId) {
+    public ResponseEntity<?> getAllProductsByRequest(@Valid @PathVariable Long productTypeId, @PathVariable Long productCategoryId,
+                                                     BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(ErrorMapper.mapErrors(bindingResult));
+        }
         try {
             List<ProductResponse> productResponse = productService.getAllProduct(productTypeId, productCategoryId);
             return ResponseEntity.ok(productResponse);
